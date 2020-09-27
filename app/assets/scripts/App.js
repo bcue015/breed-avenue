3973c5f24fc250162c7ccf0fc303d93702e11806
Bambi <bcue015@gmail.com>
2020-09-27 21:21:29 +0800

    Breed Avenue Landing Page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,13 @@ import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
+new RevealOnScroll($(".home-section__title"), "95%");
+new RevealOnScroll($(".cat-section__content"), "85%");
+new RevealOnScroll($(".dog-section__content"), "85%");
+new RevealOnScroll($(".other-pets-section__content"), "85%");
+new RevealOnScroll($(".blog-section__title"), "85%");
+new RevealOnScroll($(".blog-section__the-btn"), "85%");
+new RevealOnScroll($(".row--align-y-start"), "85%");
+new RevealOnScroll($(".site-footer__bambi"), "95%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,17 +1,12 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
-import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor() {
-		this.lazyImages = $(".lazy-load");
+		this.lazyImages = $(".lazyload");
 		this.siteHeader = $(".site-header");
-		this.headerTriggerElement = $(".large-hero__title");
+		this.headerTriggerElement = $(".large-hero__content");
 		this.createHeaderWaypoint();
-		this.pageSections = $(".page-section");
-		this.headerLinks = $(".primary-nav a");
-		this.createPageSectionWaypoints();
-		this.addSmoothScrolling();
 		this.refreshWaypoints();
 	}
 
@@ -19,10 +14,6 @@ class StickyHeader {
 		this.lazyImages.on('load', function() {
 			Waypoint.refreshAll();
 		});
-	}
-
-	addSmoothScrolling() {
-		this.headerLinks.smoothScroll();
 	}
 
 	createHeaderWaypoint() {
@@ -36,36 +27,6 @@ class StickyHeader {
 					that.siteHeader.removeClass("site-header--dark");
 				}
 			}
-		});
-	}
-
-	createPageSectionWaypoints() {
-		var that = this;
-		this.pageSections.each(function() {
-			var currentPageSection = this;
-			new Waypoint({
-				element: currentPageSection,
-				handler: function(direction) {
-					if(direction == "down") {
-						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-						that.headerLinks.removeClass("is-current-link");
-						$(matchingHeaderLink).addClass("is-current-link");
-					}
-				},
-				offset: "18%"
-			});
-
-			new Waypoint({
-				element: currentPageSection,
-				handler: function(direction) {
-					if(direction == "up") {
-						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-						that.headerLinks.removeClass("is-current-link");
-						$(matchingHeaderLink).addClass("is-current-link");
-					}
-				},
-				offset: "-40%"
-			});
 		});
 	}
 }
