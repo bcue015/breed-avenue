49ccaffffaace8530291d9129859c496e0d23752
Bambi <bcue015@gmail.com>
2020-10-23 13:31:09 +0800

    Animating on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,12 +6,11 @@ import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".home-section__title"), "95%");
-new RevealOnScroll($(".cat-section__content"), "85%");
-new RevealOnScroll($(".dog-section__content"), "85%");
-new RevealOnScroll($(".other-pets-section__content"), "85%");
-new RevealOnScroll($(".blog-section__title"), "85%");
+new RevealOnScroll($(".cat-section"), "55%");
+new RevealOnScroll($(".dog-section"), "55%");
+new RevealOnScroll($(".other-pets-section"), "55%");
+new RevealOnScroll($(".blog-section > div"), "55%");
 new RevealOnScroll($(".blog-section__the-btn"), "85%");
-new RevealOnScroll($(".row--align-y-start"), "85%");
-new RevealOnScroll($(".site-footer__bambi"), "95%");
+new RevealOnScroll($(".site-footer"), "65%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
