7284468aa1bff79a5ba1b6b349b03ac1804bd493
Bambi <bcue015@gmail.com>
2020-10-30 01:37:10 +0800

    Done About Us Page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -11,6 +11,8 @@ new RevealOnScroll($(".dog-section"), "55%");
 new RevealOnScroll($(".other-pets-section"), "55%");
 new RevealOnScroll($(".blog-section"), "45%");
 new RevealOnScroll($(".page-post"), "45%");
+new RevealOnScroll($(".page-about"), "45%");
+new RevealOnScroll($(".page-authors"), "15%");
 new RevealOnScroll($(".site-footer"), "65%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
